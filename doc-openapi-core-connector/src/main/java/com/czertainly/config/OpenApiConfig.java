f6d30eea507acebd800ac67fe173eb9bff0d94d9
Ľubomír Wassermann <76479559+lubomirw@users.noreply.github.com>
2022-11-10 08:27:55 +0100

    Fix swagger documentation annotations to reflect attributes V2 inheritance
    - fix annotations and attributes schemas
    - add version to generated API docs

--- doc-openapi-core-connector/src/main/java/com/czertainly/config/OpenApiConfig.java
+++ doc-openapi-core-connector/src/main/java/com/czertainly/config/OpenApiConfig.java
@@ -1,17 +1,25 @@
 package com.czertainly.config;
 
+import com.czertainly.api.model.common.attribute.v2.BaseAttribute;
+import com.czertainly.api.model.core.connector.FunctionGroupCode;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.ObjectSchema;
+import io.swagger.v3.oas.models.media.Schema;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.ServletContext;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
@@ -19,6 +27,9 @@ public class OpenApiConfig {
 	
 	@Autowired
 	ServletContext context;
+
+    @Value( "${api.version}" )
+    private String version;
 	
     @Bean
     public GroupedOpenApi connectorApis() {
@@ -37,6 +48,12 @@ public class OpenApiConfig {
         logoExtensionFields.put("url", "images/czertainly_color_H.svg");
         logoExtension.put("x-logo", logoExtensionFields);
 
+//        Schema<Map<String, List<BaseAttribute>>> innerSchema = new Schema<Map<String, List<BaseAttribute>>>();
+//        innerSchema.setAdditionalProperties(new ArraySchema());
+//
+//        Schema<Map<FunctionGroupCode, Map<String, List<BaseAttribute>>>> outerSchema = new Schema<Map<FunctionGroupCode, Map<String, List<BaseAttribute>>>>();
+//        outerSchema.setAdditionalProperties(innerSchema);
+
         return new OpenAPI()
                 .info(new Info().title("CZERTAINLY Connector API")
                         .description("REST API for managing Connectors in the platform")
@@ -52,6 +69,7 @@ public class OpenApiConfig {
                         .description("CZERTAINLY Documentation")
                         .url("https://docs.czertainly.com"))
                 .servers(null)
+//                .components(new Components().addSchemas("AttributesMap", outerSchema))
                 ;
     }
 
