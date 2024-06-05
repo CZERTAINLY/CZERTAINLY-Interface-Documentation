4f178a371b8504562b166a073ea4f03752cecfc1
3keyroman <roman.cinkais@3key.company>
2024-06-05 12:39:42 +0200

    Add docs for the CMP interfaces

---
+++ doc-openapi-protocol-cmp/src/main/java/com/czertainly/api/impl/CmpControllerDummyImpl.java
@@ -0,0 +1,19 @@
+package com.czertainly.api.impl;
+
+import com.czertainly.api.interfaces.core.cmp.CmpController;
+import com.czertainly.api.interfaces.core.cmp.error.CmpBaseException;
+import org.springframework.http.ResponseEntity;
+
+public class CmpControllerDummyImpl implements CmpController {
+
+    @Override
+    public ResponseEntity<byte[]> doGet(String cmpProfileName, byte[] message) throws CmpBaseException {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<byte[]> doPost(String cmpProfileName, byte[] request) throws CmpBaseException {
+        return null;
+    }
+
+}
---
+++ doc-openapi-protocol-cmp/src/main/java/com/czertainly/api/impl/CmpRaProfileControllerDummyImpl.java
@@ -0,0 +1,19 @@
+package com.czertainly.api.impl;
+
+import com.czertainly.api.interfaces.core.cmp.CmpRaProfileController;
+import com.czertainly.api.interfaces.core.cmp.error.CmpBaseException;
+import org.springframework.http.ResponseEntity;
+
+public class CmpRaProfileControllerDummyImpl implements CmpRaProfileController {
+
+    @Override
+    public ResponseEntity<byte[]> doGet(String raProfileName, byte[] message) throws CmpBaseException {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<byte[]> doPost(String raProfileName, byte[] request) throws CmpBaseException {
+        return null;
+    }
+
+}
