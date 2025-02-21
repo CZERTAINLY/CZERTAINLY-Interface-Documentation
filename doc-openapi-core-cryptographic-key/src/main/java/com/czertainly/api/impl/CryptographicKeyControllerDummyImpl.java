82712aed86d65fac419ad6f31e7f49dbee97024a
lubomirw <lubomir@wassermann.sk>
2025-02-21 11:57:04 +0100

    Add supress warnings

--- doc-openapi-core-cryptographic-key/src/main/java/com/czertainly/api/impl/CryptographicKeyControllerDummyImpl.java
+++ doc-openapi-core-cryptographic-key/src/main/java/com/czertainly/api/impl/CryptographicKeyControllerDummyImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@SuppressWarnings("java:S1186")
 public class CryptographicKeyControllerDummyImpl implements CryptographicKeyController {
     @Override
     public List<SearchFieldDataByGroupDto> getSearchableFieldInformation() {
