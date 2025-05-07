024afa04a90aad81f02352500640a9bf9ec367ca
Ľubomír Wassermann <76479559+lubomirw@users.noreply.github.com>
2025-05-07 10:29:49 +0200

    Add suppress warnings to notifications API controllers

--- doc-openapi-core-notification/src/main/java/com/czertainly/api/impl/NotificationControllerDummyImpl.java
+++ doc-openapi-core-notification/src/main/java/com/czertainly/api/impl/NotificationControllerDummyImpl.java
@@ -3,13 +3,13 @@ package com.czertainly.api.impl;
 import com.czertainly.api.exception.NotFoundException;
 import com.czertainly.api.exception.ValidationException;
 import com.czertainly.api.interfaces.core.web.NotificationController;
-import com.czertainly.api.model.client.notification.NotificationDto;
 import com.czertainly.api.model.client.notification.NotificationRequestDto;
 import com.czertainly.api.model.client.notification.NotificationResponseDto;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@SuppressWarnings("java:S1186")
 @RestController
 public class NotificationControllerDummyImpl implements NotificationController {
     @Override
--- doc-openapi-core-notification/src/main/java/com/czertainly/api/impl/NotificationInstanceControllerDummyImpl.java
+++ doc-openapi-core-notification/src/main/java/com/czertainly/api/impl/NotificationInstanceControllerDummyImpl.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@SuppressWarnings("java:S1186")
 @RestController
 public class NotificationInstanceControllerDummyImpl implements NotificationInstanceController {
 
